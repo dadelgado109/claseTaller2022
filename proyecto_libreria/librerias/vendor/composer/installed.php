3fccba057042a6b8244b954f905b77e9b3b162a1
Damian Delgado <damisintesis109@gmail.com>
2022-03-21 23:52:01 -0300

    Se sube clase 2022-03-21

---
+++ proyecto_libreria/librerias/vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '834f0fe8e0bcc6dea53d4e3050724cd1ce782729',
+    'name' => 'openm/librerias',
+  ),
+  'versions' => 
+  array (
+    'openm/librerias' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '834f0fe8e0bcc6dea53d4e3050724cd1ce782729',
+    ),
+    'phpmailer/phpmailer' => 
+    array (
+      'pretty_version' => 'v6.6.0',
+      'version' => '6.6.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'e43bac82edc26ca04b36143a48bde1c051cfd5b1',
+    ),
+  ),
+);
